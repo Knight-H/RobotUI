38722286e18fce8cb3c3df88ba9cbb65e1b7cf1b
cl21484952 <32980495+cl21484952@users.noreply.github.com>
2018-03-28 21:12:30 +0700

    Change menu item
    Now clicking/tapping on the left/right arrow will change menu but both have the same behavior

--- angular-src/src/app/components/menu/menu.component.ts
+++ angular-src/src/app/components/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  private menuStuff = null;
+  private counter:number = 0;
 
-  ngOnInit() {
+  constructor() {
+
   }
 
+  ngOnInit() {
+    this.menuStuff = [{
+      itemNo: 1,
+      itemName: 'Fried Rice',
+      itemDescription: 'Gud Fud',
+      itemPrice: 45,
+      isAvailable: 1,
+      image: 'http://i.huffpost.com/gen/1126498/images/o-LIFE-CHANGE-facebook.jpg'
+    },
+    {
+      itemNo: 2,
+      itemName: 'Fried Egg',
+      itemDescription: 'Gud Egg',
+      itemPrice: 20,
+      isAvailable: 1,
+      image: 'http://i.huffpost.com/gen/1126498/images/o-LIFE-CHANGE-facebook.jpg'
+    },
+    {
+      itemNo: 3,
+      itemName: 'Lamb Sauce',
+      itemDescription: 'Gordon Ramsy does not approve :c',
+      itemPrice: 450,
+      isAvailable: 0,
+      image: 'http://i.huffpost.com/gen/1126498/images/o-LIFE-CHANGE-facebook.jpg'
+    }];
+    this.counter = 0;
+    $("#leftarrow").click(this.nextItem.bind(this));
+    $("#rightarrow").click(this.nextItem.bind(this));
+    this.nextItem();
+    console.log("lol");
+  }
+
+  nextItem(this) {
+    this.counter = (this.counter + 1) % this.menuStuff.length;
+    let item = this.menuStuff[this.counter];
+    console.log("hi");
+    $("#itemNo").text(item.itemNo);
+    $("#itemName").text(item.itemName);
+    //$('#itemImage').attr("src", "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="); // ?????
+    $("#itemDescription").text(item.itemDescription);
+    $("#itemPrice").text(item.itemPrice + " Baht");
+    $("#isAvailable").text((item.isAvailable === 1) ? "Have" : "No Have" )
+  }
 }
