83782646000920520cabb3ae798de58552e35026
cl21484952 <chrispongleemail@gmail.com>
2018-04-21 16:30:06 +0700

    Added sim
    Able to simulate operation without database

--- angular-src/src/app/robots.service.ts
+++ angular-src/src/app/robots.service.ts
@@ -6,6 +6,7 @@ import * as $ from 'jquery';
 @Injectable()
 export class RobotsService {
 
+  private isSim: boolean = true;
   // private robotUUID = "66acd781-2bdc-4a94-a874-68e0bca95560";
 
   private robotSrc = new BehaviorSubject<any>(null);
@@ -13,15 +14,18 @@ export class RobotsService {
 
   constructor() { }
 
-  public setRobotBusy(){
+  public setRobotBusy() {
     this.setRobotStatus(2);
   }
 
-  public setRobotFree(){
+  public setRobotFree() {
     this.setRobotStatus(1);
   }
 
-  public setRobotStatus(rstatus){
+  public setRobotStatus(rstatus) {
+    if (this.isSim) {
+      return;
+    }
     $.getJSON(`http://localhost:100/robot/updateStatus?robotUUID=66acd781-2bdc-4a94-a874-68e0bca95560&rstatus=${rstatus}`);
   }
 
